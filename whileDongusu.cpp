4eb27ee2ff089bc7a23e4aeb3f461f97d39d9660
mmstfkc <36615411+mmstfkc@users.noreply.github.com>
2018-12-15 12:31:55 +0300

    while dongusu

---
+++ whileDongusu.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+	/*
+	while dongusu
+	
+	baslangis degeri
+	
+	while(kosul){
+	islemler
+	}
+	
+	*/
+	int sayac=1;
+	
+	while(sayac<10){
+		//\t bir tab lýk bosluk birakmaya yarar
+		printf("%d\t",sayac);
+		sayac++;
+		//sayac degerini birer birer arritmaya yarar
+	}
+			
+	
+	return 0;
+}
