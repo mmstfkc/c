678145cb8988cfc4cd37ff9d41379cc689d8d757
mmstfkc <36615411+mmstfkc@users.noreply.github.com>
2018-12-15 12:29:38 +0300

    Kosul

---
+++ kosul.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int i;
+	
+	printf("Bir deger giriniz");
+	scanf("%d",&i);
+	if(i==0)
+		printf("Kosul karsilandi");
+	else
+	printf("Kosul karsilanmadi");
+	
+	return 0;
+}
