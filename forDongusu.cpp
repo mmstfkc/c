abb48dc81070a503a974621fa5f053cff55eb11a
mmstfkc <36615411+mmstfkc@users.noreply.github.com>
2018-12-15 12:32:23 +0300

    for dongusu

---
+++ forDongusu.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	/*
+	for dongusu
+	
+	for(baslangic degeri;kosul;artis miktari){
+	islmeler
+	}	
+	*/
+	int sayac;
+	
+	for(sayac=1;sayac<10;sayac++){
+		printf("%d\t",sayac);
+	}
+	
+	
+	return 0;
+}
